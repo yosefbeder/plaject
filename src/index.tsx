9b9c153219b572b41a43e3fd17384dc1c69a2478
yosefbeder <dryosefbeder@gmail.com>
2021-07-06 07:17:57 +0200

    undo ejecting

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux';
 import { store } from './store';
 
 ReactDOM.render(
-  <Router>
+  <Router basename="/plaject">
     <Provider store={store}>
       <React.StrictMode>
         <App />
