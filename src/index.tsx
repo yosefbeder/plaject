3e5badd1a3eb96ea01478eb016fd59507c356d8e
yosefbeder <dryosefbeder@gmail.com>
2021-07-22 08:25:39 +0200

    Solved some issues with routing

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
 import './global-styles.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store';
 
 ReactDOM.render(
-  <Router basename="/plaject">
+  <Router basename="/">
     <Provider store={store}>
       <React.StrictMode>
         <App />
